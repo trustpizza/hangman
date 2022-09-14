a7020df48a6055cbceac8f4c135fb41bfc987fab
Axel <108423732+trustpizza@users.noreply.github.com>
2022-09-14 13:50:30 -0500

    Update to randomly select words from dictionary
    Words are exactly 1 extra empty space long.  Work on fixing the root cause.  Look at the blankify and deblankify functions as a starting point

--- game.rb
+++ game.rb
@@ -1,6 +1,9 @@
 require 'pry-byebug'
+
+words_sampled = File.read('google-10000-no-swears.txt')
+
 class Game
-  attr_accessor :word, :word_to_array, :rounds_left, :progress, :win, :lose
+  attr_accessor :word, :word_to_array, :rounds_left, :progress, :win, :lose, :alphabet
 
   def initialize(word)
     @word = word
@@ -9,17 +12,18 @@ class Game
     @guess = ''
     @game_over = false
     @win = check_win(@word, @blank_word, @game_over)
-    @rounds_left = 6
+    @rounds_left = 10
     @progress = de_blankify(@blank_word, @word_to_array, @guess)
+    @alphabet = ('a'..'z').to_a
   end
 
-  def blankify(word)
+  def blankify(_word)
     out = ''
     @word.length.times { out += '_' }
     out
   end
 
-  def de_blankify(blank_word, word_to_array, guess)
+  def de_blankify(_blank_word, _word_to_array, _guess)
     @word_to_array.each_with_index do |letter, index|
       @blank_word[index] = @guess if letter == @guess
     end
@@ -31,7 +35,7 @@ class Game
     out = gets.chomp.downcase.split('')[0]
   end
 
-  def check_win(word, blank_word, game_over)
+  def check_win(_word, _blank_word, _game_over)
     if @blank_word == @word
       puts 'You Win!'
       @game_over = true
@@ -39,19 +43,24 @@ class Game
     @game_over
   end
 
-  def check_lose(rounds_left, win)
+  def check_lose(_rounds_left, _win, word)
     out = false
     if @rounds_left.zero? && @win == false
       puts 'You Lose!'
       out = true
     end
+    puts "The word was #{@word}"
     out
   end
 
-  def play(word, word_to_array, blank_word, rounds_left, progress, win, lose)
+  def play(_word, word_to_array, blank_word, _rounds_left, _progress, _win, _lose, alphabet)
     until @win == true || @lose == true
-      @guess = get_guess()
-
+      puts "Letters Remaining"
+      puts @alphabet.join(' ')
+      
+      @guess = get_guess
+      @alphabet.delete(@guess)
+      
       @progress = de_blankify(blank_word, word_to_array, @guess)
       puts @progress
 
@@ -60,13 +69,19 @@ class Game
 
       @win = check_win(@blank_word, @word, @game_over)
 
-      @lose = check_lose(@rounds_left, @win)
+      @lose = check_lose(@rounds_left, @win, @word)
     end
   end
 end
 
-new_game = Game.new('grace')
-new_game.play(@word, @word_to_array, @blank_word, @rounds_left, @progress, @win, @lose)
+words_to_sample = []
 
+allowed_words = File.readlines('google-10000-no-swears.txt')
+allowed_words.each do |word|
+  unless word.length < 5 || word.length > 12
+    words_to_sample.push(word)
+  end
+end
 
-
+new_game = Game.new(words_to_sample.sample)
+new_game.play(@word, @word_to_array, @blank_word, @rounds_left, @progress, @win, @lose, @alphabet)
